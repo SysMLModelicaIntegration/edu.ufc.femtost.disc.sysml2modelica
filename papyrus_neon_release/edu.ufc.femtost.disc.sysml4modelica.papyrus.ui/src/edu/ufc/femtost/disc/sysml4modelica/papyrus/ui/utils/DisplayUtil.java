870e5b32e8ce4ede9e3ac9ca1e91da718d842bd1
Gauthier <jm.gauthier@samares-engineering.com>
2016-12-08 10:59:53 +0100

    Neon migration

--- papyrus_neon_release/edu.ufc.femtost.disc.sysml4modelica.papyrus.ui/src/edu/ufc/femtost/disc/sysml4modelica/papyrus/ui/utils/DisplayUtil.java
+++ papyrus_neon_release/edu.ufc.femtost.disc.sysml4modelica.papyrus.ui/src/edu/ufc/femtost/disc/sysml4modelica/papyrus/ui/utils/DisplayUtil.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2013 Jean-Marie Gauthier and University of Franche-Comte
+ * Copyright (c) 2013-2016 Jean-Marie Gauthier, University of Franche-Comte, and Samares-Engineering
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Jean-Marie Gauthier and University of Franche-Comte - initial API and implementation
+ * Jean-Marie Gauthier, University of Franche-Comte - initial API and implementation
+ * Jean-Marie Gauthier, Samares-Engineering - Neon implementation
  *******************************************************************************/
 package edu.ufc.femtost.disc.sysml4modelica.papyrus.ui.utils;
 
